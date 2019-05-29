fcee937a68a3590db095e6d4c2d5ce8288433f16
mijdavis2 <mdavisinsc@gmail.com>
2019-05-29 20:48:35 +0100

    Set TZ to Los_Angeles

--- docker_run.sh
+++ docker_run.sh
@@ -2,12 +2,14 @@
 
 # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
 
+docker pull pihole/pihole:latest
+
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
     -p 443:443 \
-    -e TZ="America/Chicago" \
+    -e TZ="America/Los_Angeles" \
     -v "$(pwd)/etc-pihole/:/etc/pihole/" \
     -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
     --dns=127.0.0.1 --dns=1.1.1.1 \
