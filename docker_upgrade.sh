35b9c6def05062f6a158aaebc957712db45574a0
mijdavis2 <mdavisinsc@gmail.com>
2019-05-29 20:48:07 +0100

    Cleanup docker upgrade output

--- docker_upgrade.sh
+++ docker_upgrade.sh
@@ -9,26 +9,14 @@
 
 docker pull pihole/pihole
 
+echo "Removing old pihole container..."
+docker -l fatal stop pihole 
+docker -l fatal rm pihole
+echo "Ready to deploy new pihole container"
 
-echo -e "\n\nRemoving old pihole container..."
-docker stop pihole
-docker rm pihole
+./docker_run.sh 2> /dev/null
 
-cat << EOF 
-
-#####
-Pihole is now temporarily down as well as DNS if there is no fallback.
-
-Ignore following error when docker tries to pull the latest pihole image.
-We already did that while the old pihole was running.
-#####
-
-EOF
-
-./docker_run.sh
-
-echo -e "\n\nStarting docker image cleanup, may ignore errors..."
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
-echo "Docker image cleanup complete"
-
+echo "Removing old docker images..."
+docker -l fatal rmi $(docker images --filter "dangling=true" -q --no-trunc) 2> /dev/null
+docker -l fatal rmi $(docker images | grep "none" | awk '/ / { print $3 }') 2> /dev/null
+echo "Cleanup complete"
